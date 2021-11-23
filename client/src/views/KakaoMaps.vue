66e94465a4ca91b87386031ee09916351ed0c151
JongGeun <hamburgermax5@gmail.com>
2021-11-23 10:53:05 +0900

    상세인포 마커 추가, 위치에러있음

--- client/src/views/KakaoMaps.vue
+++ client/src/views/KakaoMaps.vue
@@ -189,7 +189,6 @@ export default {
 
               // 마커 이미지를 생성합니다
               var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-              console.log(result[0].y, result[0].x);
 
               maxX = Math.max(maxX, parseFloat(result[0].x));
               minX = Math.min(minX, parseFloat(result[0].x));
@@ -200,20 +199,61 @@ export default {
               var marker = new kakao.maps.Marker({
                 map: map, // 마커를 표시할 지도
                 position: new kakao.maps.LatLng(result[0].y, result[0].x), // 마커를 표시할 위치
-                title: house.단지명, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image: markerImage, // 마커 이미지
               });
-              if (cnt == 1) {
-                map.setCenter(new kakao.maps.LatLng(maxY, maxX));
-              } else if (cnt > 1) {
-                map.setCenter(
-                  new kakao.maps.LatLng((maxY + minY) / 2, (maxX + minX) / 2)
-                );
+
+              // 커스텀 오버레이에 표시할 컨텐츠 입니다
+              // 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+              // 별도의 이벤트 메소드를 제공하지 않습니다
+              // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+
+              var content =
+                '<div class="wrap">' +
+                '    <div class="info">' +
+                '        <div class="title">' +
+                "            카카오 스페이스닷원" +
+                '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+                "        </div>" +
+                '        <div class="body">' +
+                '            <div class="img">' +
+                '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+                "           </div>" +
+                '            <div class="desc">' +
+                '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+                '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+                '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+                "            </div>" +
+                "        </div>" +
+                "    </div>" +
+                "</div>";
+
+              // 마커 위에 커스텀오버레이를 표시합니다
+              // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+              var overlay = new kakao.maps.CustomOverlay({
+                content: content,
+                map: map,
+                position: marker.getPosition(),
+              });
+              console.log(marker.getPosition());
+              // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+              kakao.maps.event.addListener(marker, "click", function () {
+                overlay.setMap(map);
+              });
+
+              function closeOverlay() {
+                overlay.setMap(null);
               }
+            }
+            if (cnt == 1) {
+              map.setCenter(new kakao.maps.LatLng(maxY, maxX));
+            } else if (cnt > 1) {
+              map.setCenter(
+                new kakao.maps.LatLng((maxY + minY) / 2, (maxX + minX) / 2)
+              );
             }
           }
         );
       });
+
       // 카테고리별 장소 띄우기
 
       // 마커를 클릭했을 때 해당 장소의 상세정보를 보여줄 커스텀오버레이입니다
@@ -571,5 +611,98 @@ export default {
   position: relative;
   width: 100%;
   height: 350px;
+}
+/* 마커 커스텀 */
+.wrap {
+  position: absolute;
+  left: 0;
+  bottom: 40px;
+  width: 288px;
+  height: 132px;
+  margin-left: -144px;
+  text-align: left;
+  overflow: hidden;
+  font-size: 12px;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  line-height: 1.5;
+}
+.wrap * {
+  padding: 0;
+  margin: 0;
+}
+.wrap .info {
+  width: 286px;
+  height: 120px;
+  border-radius: 5px;
+  border-bottom: 2px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+  background: #fff;
+}
+.wrap .info:nth-child(1) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.info .title {
+  padding: 5px 0 0 10px;
+  height: 30px;
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-size: 18px;
+  font-weight: bold;
+}
+.info .close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #888;
+  width: 17px;
+  height: 17px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png");
+}
+.info .close:hover {
+  cursor: pointer;
+}
+.info .body {
+  position: relative;
+  overflow: hidden;
+}
+.info .desc {
+  position: relative;
+  margin: 13px 0 0 90px;
+  height: 75px;
+}
+.desc .ellipsis {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.desc .jibun {
+  font-size: 11px;
+  color: #888;
+  margin-top: -2px;
+}
+.info .img {
+  position: absolute;
+  top: 6px;
+  left: 5px;
+  width: 73px;
+  height: 71px;
+  border: 1px solid #ddd;
+  color: #888;
+  overflow: hidden;
+}
+.info:after {
+  content: "";
+  position: absolute;
+  margin-left: -12px;
+  left: 50%;
+  bottom: 0;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.info .link {
+  color: #5085bb;
 }
 </style>
