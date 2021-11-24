e91e51b8dd0ad693c46cef3bcb61693c9f65dda1
JongGeun <hamburgermax5@gmail.com>
2021-11-24 11:10:51 +0900

    house Object add 시군구

--- client/src/util/houseObject.js
+++ client/src/util/houseObject.js
@@ -47,12 +47,13 @@ export function makeHouseSimpleObject(house, houseType) {
     }
   } else {
     houseObject.도로명 = house.도로명;
-    houseObject.연면적 = house.연면적;
-    houseObject.대지면적 = house.대지면적;
+
     if (house.거래금액) {
       houseObject.거래금액 = house.거래금액;
     }
     if (house.전월세구분) {
+      houseObject.연면적 = house.연면적;
+      houseObject.대지면적 = house.대지면적;
       houseObject.구분 = house.전월세구분;
       if (house.전월세구분 === `전세`) {
         houseObject.보증금 = house.보증금;
@@ -83,13 +84,16 @@ export function makeHouseDetailObject(house, houseType) {
   } else if (houseType === `office`) {
     houseObject.이름 = house.단지명;
     houseObject.도로명 = house.도로명;
+    houseObject.시군구 = house.시군구;
   } else if (houseType === `yeonrip`) {
     houseObject.이름 = house.건물명;
     houseObject.도로명 = house.도로명;
+    houseObject.시군구 = house.시군구;
   } else {
     houseObject.도로명 = house.도로명;
     houseObject.연면적 = house.연면적;
     houseObject.대지면적 = house.대지면적;
+    houseObject.시군구 = house.시군구;
   }
   return houseObject;
 }
